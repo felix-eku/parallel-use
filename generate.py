80f38481a37d6de822900113d725f791d69b5c01
Felix Kunzmann <fe.kunzmann@gmail.com>
2022-04-30 20:17:14 +0200

    Add default values for time parameters of generate_orders.

--- generate.py
+++ generate.py
@@ -5,8 +5,9 @@ import pandas as pd
 
 
 def generate_orders(
-    n: int, start: datetime, step: timedelta, deviation: timedelta, max_time: timedelta,
-    seed: int = 0
+    n: int, 
+    start: datetime = None, step: timedelta = None, deviation: timedelta = None, 
+    max_time: timedelta = None, seed: int = 0
 ):
     """
     Generate a pandas.DataFrame, the rows of which represent random orders.
@@ -15,6 +16,15 @@ def generate_orders(
     the column "preferred_start" the preferred start time
     and the column "processing_time" the processing time.
     """
+    if start is None:
+        start = datetime.now()
+    if step is None:
+        step = timedelta(hours=6)
+    if deviation is None:
+        deviation = timedelta(hours=4)
+    if max_time is None:
+        max_time = timedelta(hours=24)
+
     rng = default_rng(seed)
     return pd.DataFrame(
         {
